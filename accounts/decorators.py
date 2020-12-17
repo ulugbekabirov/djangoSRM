a36ec9d31a50f6c5c033e779ac11f84554eb95ef
ulugbekabirov <ulugbekabirov@gmail.com>
2020-12-17 18:29:23 +0500

    bug with user who has multiple groups

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -15,12 +15,17 @@ def unauthenticated_user(view_func):
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
-            # group = None
+
             if request.user.groups.exists():
-                group = request.user.groups.all()[0].name
-                if group in allowed_roles:
+                groups = request.user.groups.all()
+                if any(group.name in allowed_roles for group in groups):
+                    # If user is in any of allowed groups
                     return view_func(request, *args, **kwargs)
                 return HttpResponse("Not allowed")
+
+            messages.info(request, "That User does not belong to any group")
+            return redirect("logout")
+
         return wrapper_func
     return decorator
 
