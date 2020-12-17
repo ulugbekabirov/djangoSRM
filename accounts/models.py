a4ec38120b4e3838b9e87c25466397be351d28b0
ulugbekabirov <ulugbekabirov@gmail.com>
2020-12-17 12:03:49 +0500

    user field in customer added

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
 class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=30, null=True)
     phone = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=50, null=True)
