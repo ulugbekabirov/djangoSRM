f885d247fdc743e76726ef7fe0a3d02cd3fedc80
ulugbekabirov <ulugbekabirov@gmail.com>
2020-12-18 11:41:27 +0500

    added profile pic for cust omer

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=50, null=True)
     date_created = models.DateField(auto_now_add=True)
+    profile_pic = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
